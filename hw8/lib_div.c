2fd1a9009b764debb713fc1615a2850a31cdcdea
mariarybina <ribinmaria@gmail.com>
2024-09-05 13:19:25 +0700

    hw8

---
+++ hw8/lib_div.c
@@ -0,0 +1,10 @@
+int div(int a, int b)
+{
+    if (0 == b)
+    {
+        return -1;
+    }
+
+    int div = a / b;
+    return div;
+}
---
+++ hw8/lib_sub.c
@@ -0,0 +1,5 @@
+int sub(int a, int b)
+{
+    int sub = a - b;
+    return sub;
+}
