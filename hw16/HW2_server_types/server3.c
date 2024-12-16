e7975f6d06a1167250de4438cb0f7414d85464e7
mariarybina <ribinmaria@gmail.com>
2024-12-16 21:36:47 +0700

    hw166

--- hw16/HW2_server_types/server3.c
+++ hw16/HW2_server_types/server3.c
@@ -20,47 +20,62 @@
 
 struct Data
 { 
-    int *p_client_socket;
     mqd_t *p_qd;
 };
 
-
-/*static void *func()
+static void *func(void *args)
 {
     int ret = 0;
     int prio = 0;
+    int client_socket = 0;
     char message[BUFSIZE] = {0};
-    char socket[] = {0};
+    char socket[MESSAGESIZE] = {0};
     char server_message[10] = {"Hello!"};
+
+    struct Data *t_data = (struct Data *)args;
+    if (!t_data)
+    {
+        return NULL;
+    }
 
     while(1)
     {
-        ret = mq_receive(qd, socket, MESSAGESIZE, &prio)
+        ret = mq_receive(*t_data->p_qd, socket, MESSAGESIZE, &prio);
         if (-1 == ret)
         {
             perror("Message receive failed");
-            return -1;
+            break;
         }
 
-        ret = recv(socket, message, sizeof(message), 0);
+        printf("\nreceived socket number %d\n", *t_data->p_qd);
 
-        ret = send(socket, server_message, sizeof(server_message), 0);
+        int client_socket = (int)socket[0];
+
+        ret = recv(client_socket, message, sizeof(message), 0);
+        if (-1 == ret)
+        {
+            perror("Reception from client failed.");
+            close(client_socket);
+            break;
+        }
+
+        ret = send(client_socket, server_message, sizeof(server_message), 0);
+        if (-1 == ret)
+        {
+            perror("Sending to client failed");
+            close(client_socket);
+            break;
+        }
     }
-
-}*/
+}
 
 int main()
 {
     int server_socket = 0;
-    int client_socket = 0;
     int ret = 0;
-    
     mqd_t qd = 0;
-    //int prio = 0;
 
-    //struct Data data[THREADS] = {0};
-
-
+    struct Data data[THREADS] = {0};
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (-1 == server_socket)
@@ -89,12 +104,16 @@ int main()
         close(server_socket);
         return -1;
     }
-
-    
     
     struct mq_attr queue_attr;
     queue_attr.mq_msgsize = MESSAGESIZE;
     queue_attr.mq_maxmsg = MESSAGENUMBER;
+
+    ret = mq_unlink(QUEUE_NAME);
+    if (-1 == ret)
+    {
+        perror("Queue already removed");
+    }
 
     qd = mq_open(QUEUE_NAME, O_CREAT | O_RDWR, 0600, &queue_attr);
     if (-1 == qd)
@@ -103,51 +122,42 @@ int main()
         return -1;
     }
 
-    /*for (int i = 0; i < THREADS; i++)
+    for (int i = 0; i < THREADS; i++)
     {
         data[i].p_qd = &qd;
-    }*/
+    }
 
+    pthread_t tid[THREADS] = {0};
+    int status[THREADS] = {1, 1, 1, 1};
+
+    for (int i = 0; i < THREADS; i++)
+    {
+        ret = pthread_create(&tid[i], NULL, func, &data[i]);
+        if (-1 == ret)
+        {
+            perror("pthread_create() failed");
+            return -1;
+        }
+    }
 
     int sock = 0;
-    sock = accept(server_socket, NULL, 0);
-    if (-1 == sock)
+
+    while(1)
     {
-        perror("Accepting client failed.");
-    }
+        sock = accept(server_socket, NULL, 0);
+        if (-1 == sock)
+        {
+            perror("Accepting client failed.");
+        }
 
-    char ssock[1] = {(char)sock};
+        char sock_buf[1] = {(char)sock};
 
-    ret = mq_send(qd, ssock, MESSAGESIZE, 1);
-    if (-1 == ret)
-    {
-        perror("Sending message failed");
-        return -1;
-    }
-
-
-    int prio = 0;
-    char message[BUFSIZE] = {0};
-    char socket[MESSAGESIZE] = {0};
-    char server_message[10] = {"Hello!"};
-
-    ret = mq_receive(qd, socket, MESSAGESIZE, &prio);
-    if (-1 == ret)
-    {
-        perror("Message receive failed");
-        return -1;
-    }
-
-    client_socket = (int)socket[0];
-
-    ret = recv(client_socket, message, sizeof(message), 0);
-
-    ret = send(client_socket, server_message, sizeof(server_message), 0);
-
-    ret = mq_unlink(QUEUE_NAME);
-    if (-1 == ret)
-    {
-        perror("Queue already removed");
+        ret = mq_send(qd, sock_buf, MESSAGESIZE, 1);
+        if (-1 == ret)
+        {
+            perror("Sending message failed");
+            return -1;
+        }
     }
     
     return 0;
